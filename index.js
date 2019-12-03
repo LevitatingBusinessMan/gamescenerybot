5cfe0c797b6e6bc041db2b87167e0543de7e8714
Rein F <public@reinfernhout.xyz>
2019-12-03 01:43:52 +0100

    base comment ratelimit system

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const config = require(require("path").join(__dirname, "/config/config.js"))
 
 const request = require("request")
-const sizeOf = require('image-size');
-let foo = require("foowrap");
-let r = new foo(config);
+const sizeOf = require('image-size')
+let foo = require("foowrap")
+let r = new foo(config)
 
 let x = r.submissionStream({
-    sub: ["gamescenery", "testingground4bots"],
+    sub: ["testingground4bots"],
     rate: 5000
-});
+})
 
-//x.on("post", handleSubmission);
+x.on("post", handleSubmission)
 
-r.getSubmission("e57hpp").fetch().then(handleSubmission)
+//r.getSubmission("e57hpp").fetch().then(handleSubmission)
 
+const delayedComments = []
+var timeout = false
 function handleSubmission(post) {
     if (post.post_hint !== "image")
-        return;
+        return
         
     console.log(`[${post.id}] Requesting ${post.url}`)
     request({url: post.url, encoding: null}, (error, response, buffer) => {
@@ -25,16 +27,17 @@ function handleSubmission(post) {
         
         console.log(`[${post.id}] Calculating size`)
         var size = sizeOf(buffer)
-        comment(post, size);
+        
+        if (timeout) {
+            delayedComments.push({post, size})
+        } else comment({post, size})
     })
 
 }
 
-const delayedComments = [];
-
-function comment(post, size) {
-    const splitTitle = post.title.split("]");
-    const title = splitTitle[splitTitle.length-1];
+function comment({post, size}) {
+    const splitTitle = post.title.split("]")
+    const title = splitTitle[splitTitle.length-1]
     
     const replyText =
 `
@@ -62,17 +65,24 @@ ${size.width} | ${size.height}
                 const minutesLeft = err.message.substring(
                     err.message.lastIndexOf("in ") + 3, 
                     err.message.lastIndexOf(" minutes")
-                );
+                )
                 console.log(`[${post.id}] Delayed (${minutesLeft}m)`)
+
+                //try posting in x minutes + 1
+                setTimeout(handleComments, (parseInt(minutesLeft)+1)*60*1000)
+                timeout = true
             }
         }
         else console.error(err)
     })
 }
 
-setInterval(() => {
+function handleComments(){
     if (delayedComments.length) {
-        delayedComment = delayedComments[0];
-        comment(delayedComment.post, delayedComment.size).then(() => delayedComments.shift())
+        delayedComment = delayedComments[0]
+        comment(delayedComment).then(() => {
+            delayedComments.shift()
+            timeout = false
+        })
     }
-}, 60000)
+}    
